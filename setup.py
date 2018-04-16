42dfa7ae958d5f871e36f0c17acfef837fb6a239
alex <reddalexx@gmail.com>
2018-04-16 17:42:07 +0300

    CS-1244: fix datefinder dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Information Technology',
