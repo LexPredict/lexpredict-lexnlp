f58cee94baa08ca9f9c66676770bfafbcc681a60
alex <reddalexx@gmail.com>
2019-03-04 13:52:30 +0300

    fixed version

--- lexnlp/extract/en/acts.py
+++ lexnlp/extract/en/acts.py
@@ -6,11 +6,7 @@ from lexnlp.extract.en.utils import NPExtractor
 __author__ = "ContraxSuite, LLC; LexPredict, LLC"
 __copyright__ = "Copyright 2015-2019, ContraxSuite, LLC"
 __license__ = "https://github.com/LexPredict/lexpredict-lexnlp/blob/master/LICENSE"
-<<<<<<< HEAD
-__version__ = "0.2.4"
-=======
 __version__ = "0.2.5"
->>>>>>> 0.2.5
 __maintainer__ = "LexPredict, LLC"
 __email__ = "support@contraxsuite.com"
 
