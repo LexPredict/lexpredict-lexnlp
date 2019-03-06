370590b717df6c88cc5856880619e9761305987c
alex <reddalexx@gmail.com>
2019-03-06 23:28:12 +0300

    get rid of POS tagging in acts parser

--- lexnlp/extract/en/acts.py
+++ lexnlp/extract/en/acts.py
@@ -1,7 +1,5 @@
 import regex as re
 
-from lexnlp.extract.en.utils import NPExtractor
-
 
 __author__ = "ContraxSuite, LLC; LexPredict, LLC"
 __copyright__ = "Copyright 2015-2019, ContraxSuite, LLC"
@@ -11,40 +9,32 @@ __maintainer__ = "LexPredict, LLC"
 __email__ = "support@contraxsuite.com"
 
 
-act_grammar = r"""
-    NBAR:
-        {<NN.*|JJ.*|VBN|VBG|CD|\(|\)|,>*<NNP.*>}  # Nouns, Adj-s, brackets, terminated with Nouns
-    IN:
-        {<CC|IN|,>}   # &, and, of
-    SECTION:
-        {<JJ|NN><CD><IN><DT>}
-    NP:
-        {(<SECTION>)?(<NBAR><IN>+)*<NBAR>(<IN><CD>)?}
-"""
-
-ACT_NPE = NPExtractor(act_grammar)
-ACT_NPE.exception_sym += ['And', 'Of']
-ACT_NPE.exception_pos = ['CC']
-ACT_RE = re.compile(r'.+?\s+Act(?:\W|$)(?:of\s+\d{4})?')
-ACT_PARTS_RE = re.compile(r'(?P<text>(?:section (?P<section>[^\s]+)\s+of\s+the\s+)?(?P<act_name>(?:(?:[A-Z]\w+|[A-Z&]+|and|\d+(?:[a-z]{1,3})?),?\s*)+(?<=\s)Act)(?:\W|$)(?:of (?P<year>\d{4}))?)')
+ACT_PARTS_RE = re.compile(r'''
+(?P<text>
+    (?:sections?\s+
+        (?P<section>(?:\d+(?:\(\w\))*|,\s+|,?\s+and\s+)+)\s+of\s+the\s+
+    )?
+    (?P<act_name>
+        (?:(?:[A-Z]\w+|[A-Z&]+|and|\d+(?:[a-z]{1,3})?),?\s*)*
+        (?<=\s)Act
+    )
+    (?:\W+|$)
+    (?:of\s+(?P<year>\d{4}))?
+)''', re.VERBOSE|re.MULTILINE)
 
 
 def get_acts(text):
-    # may contain multiple Act like "Doe and Jones Act (D&J Act)"
-    act_name_containers = [i for i in set(ACT_NPE.get_np(text)) if ACT_RE.search(i)]
-
-    for act_name_container in act_name_containers:
-        for act_name in ACT_RE.findall(act_name_container):
-            act_name_re = re.compile(re.escape(act_name))
-            captures = ACT_PARTS_RE.search(act_name).capturesdict()
-            for match in act_name_re.finditer(text):
-                location_start, location_end = match.span()
-                yield {'location_start': location_start,
-                       'location_end': location_end,
-                       'act_name': ''.join(captures.get('act_name') or []),
-                       'section': ''.join(captures.get('section') or []),
-                       'year': ''.join(captures.get('year') or []),
-                       'value': ''.join(captures.get('text') or [])}
+    for match in ACT_PARTS_RE.finditer(text):
+        captures = match.capturesdict()
+        location_start, location_end = match.span()
+        act_name = ''.join(captures.get('act_name') or [])
+        yield {'location_start': location_start,
+               'location_end': location_end,
+               'act_name': act_name,
+               'section': ''.join(captures.get('section') or []),
+               'year': ''.join(captures.get('year') or []),
+               'ambiguous': act_name == 'Act',
+               'value': ''.join(captures.get('text') or [])}
 
 
 def get_act_list(*args, **kwargs):
--- lexnlp/extract/en/tests/test_acts.py
+++ lexnlp/extract/en/tests/test_acts.py
@@ -15,41 +15,46 @@ class TestGetActs(AssertionMixin):
         text = "This is awesome but VERY Important Act in the sentence"
         res = get_act_list(text)
         self.assertEqual(res, [{'location_start': 20,
-                                'location_end': 38,
+                                'location_end': 39,
                                 'section': '',
                                 'year': '',
+                                'ambiguous': False,
                                 'act_name': 'VERY Important Act',
-                                'value': 'VERY Important Act'}])
+                                'value': 'VERY Important Act '}])
         text = "This sentence which ends with VERY Important Act"
         res = get_act_list(text)
         self.assertEqual(res, [{'location_start': 30,
                                 'location_end': 48,
                                 'section': '',
                                 'year': '',
+                                'ambiguous': False,
                                 'act_name': 'VERY Important Act',
                                 'value': 'VERY Important Act'}])
         text = "This sentence which ends with point after VERY Important Act."
         res = get_act_list(text)
         self.assertEqual(res, [{'location_start': 42,
-                                'location_end': 60,
+                                'location_end': 61,
                                 'section': '',
                                 'year': '',
+                                'ambiguous': False,
                                 'act_name': 'VERY Important Act',
-                                'value': 'VERY Important Act'}])
+                                'value': 'VERY Important Act.'}])
         text = "test verb in phrase Frozen VERY Important Act."
         res = get_act_list(text)
-        self.assertEqual(res, [{'location_start': 13,
-                                'location_end': 45,
+        self.assertEqual(res, [{'location_start': 20,
+                                'location_end': 46,
                                 'section': '',
                                 'year': '',
+                                'ambiguous': False,
                                 'act_name': 'Frozen VERY Important Act',
-                                'value': 'Frozen VERY Important Act'}])
+                                'value': 'Frozen VERY Important Act.'}])
         text = "test year in VERY Important Act of 1954."
         res = get_act_list(text)
         self.assertEqual(res, [{'location_start': 13,
                                 'location_end': 39,
                                 'section': '',
                                 'year': '1954',
+                                'ambiguous': False,
                                 'act_name': 'VERY Important Act',
                                 'value': 'VERY Important Act of 1954'}])
         text = "test section 12 of the VERY Important Act of 1954."
@@ -58,16 +63,29 @@ class TestGetActs(AssertionMixin):
                                 'location_end': 49,
                                 'section': '12',
                                 'year': '1954',
+                                'ambiguous': False,
                                 'act_name': 'VERY Important Act',
                                 'value': 'section 12 of the VERY Important Act of 1954'}])
         text = "test that part 12 of the VERY Important Act of 1954."
         res = get_act_list(text)
-        self.assertEqual(res, [{'location_start': 10,
+        self.assertEqual(res, [{'location_start': 25,
                                 'location_end': 51,
                                 'section': '',
                                 'year': '1954',
+                                'ambiguous': False,
                                 'act_name': 'VERY Important Act',
                                 'value': 'VERY Important Act of 1954'}])
+
+    def test_ambiguous_cases(self):
+        text = 'accordance with sections 751(a)(1) and 777(i)(1) of the Act, and 19 CFR 351'
+        res = get_act_list(text)
+        self.assertEqual(res, [{'location_start': 16,
+                                'location_end': 61,
+                                'act_name': 'Act',
+                                'section': '751(a)(1) and 777(i)(1)',
+                                'year': '',
+                                'ambiguous': True,
+                                'value': 'sections 751(a)(1) and 777(i)(1) of the Act, '}])
 
     def test_wrong_cases(self):
         text = "This is awesome but incorrect Important Activity in the sentence"
