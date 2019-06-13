cd2fb32553de7080a77d6292e8b91a6ea00bfcf8
alex <reddalexx@gmail.com>
2019-06-13 21:29:22 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -89,7 +89,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'datefinder @ https://github.com/LexPredict/datefinder/archive/master.zip',
+        # 'datefinder @ https://github.com/LexPredict/datefinder/archive/master.zip',
+        'datefinder-lexpredict==0.6.2',
         'dateparser==0.7.0',
         'gensim==3.4.0',
         'nltk==3.2.4',
